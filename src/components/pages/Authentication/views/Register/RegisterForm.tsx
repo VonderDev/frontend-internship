8750396e9975baf8527f355da8c6a8213615cc00
Voragun Supawong <voragun2543@gmail.com>
2021-07-01 17:39:38 +0700

    :star2:[completed] conect backend using axios :star2:

--- src/components/pages/Authentication/views/Register/RegisterForm.tsx
+++ src/components/pages/Authentication/views/Register/RegisterForm.tsx
@@ -7,12 +7,17 @@ import { ButtonStyle, InputStyle } from "shared/style/theme/component";
 import { FormItemTextValidate, FormLogin } from "../../shared/style";
 import axios from "axios";
 import { error } from "console";
+import { stringify } from "querystring";
 
 export const RegisterForm = () => {
 
     const history = useHistory();
     const [userData, setUserData] = useState<ILogin>({ firstName: '', lastName: '', email: '',  username: '', password: ''});
-
+    const [username, setUsername] = useState("");
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
     
     // async function registerUser() {
     //     let user = {username, firstName, lastName, email, password}
@@ -33,54 +38,48 @@ export const RegisterForm = () => {
     async function API_register() {
         // let data = {username, firstName, lastName, email, password}
         // console.warn(data);
-            API_PostDataUser(userData);
-
-        // const data = await axios.post("http://localhost:5000/signup", userData)
-        //     .then((res) => {
-        //         console.log(res.data);
-        //         console.log("Go to home");
-        //         history.push("/");
-        //     })
-        //     .catch((error) => {
-        //         console.log(error.response.data.error)
-        //         console.log("Failed");
-        //         return error.response.message
-        //     });
-    }
-
-    const handleOnChange = (name : string, value : string) => {
-        setUserData((prev) => ({...prev , [name] : value}));
-    }
-
-    function postUserData(username : string, firstName : string, lastName : string, email : string, password : string) {
-        console.log(username)
-        console.log(firstName)
-        console.log(lastName)
-        console.log(password)
-        setUserData({ username, firstName, lastName, email, password })
+            // API_PostDataUser(userData);
+        let userData = {
+            username : username,
+            firstName : firstName,
+            lastName : lastName,
+            email : email,
+            password : password
+        }
+        const data = await axios.post("http://localhost:5000/signup", userData)
+            .then((res) => {
+                console.log(res.data);
+                console.log("Go to home");
+                history.push("/");
+            })
+            .catch((error) => {
+                console.log(error.response)
+                console.log("Failed");
+                return error.response.message
+            });
     }
 
     return (
 
         <FormLogin layout="horizontal">
             <FormItemTextValidate name="username" rules={[{ required: true, message: 'กรุณากรอกชื่อผู้ใช้' }]} >
-                <InputStyle sizeinput={100} type="text" value={userData.username} placeholder="ชื่อผู้ใช้" onChange={({ target: { value, name } }) => { handleOnChange(name, value); }} />
+                <InputStyle sizeinput={100} type="text" value={username} placeholder="ชื่อผู้ใช้" onChange={({ target: { value } }) => { setUsername(value); }} />
             </FormItemTextValidate>
 
             <FormItemTextValidate name="firstname" rules={[{ required: true, message: 'กรุณากรอกชื่อจริง' }]} >
-                <InputStyle sizeinput={100} type="text" value={userData.firstName} placeholder="ชื่อจริง" onChange={({ target: { value, name } }) => { handleOnChange(name, value) }} />
+                <InputStyle sizeinput={100} type="text" value={firstName} placeholder="ชื่อจริง" onChange={({ target: { value } }) => { setFirstName(value) }} />
             </FormItemTextValidate>
 
             <FormItemTextValidate name="lastname" rules={[{ required: true, message: 'กรุณากรอกนามสกุล' }]} >
-                <InputStyle sizeinput={100} type="text" value={userData.lastName} placeholder="นามสกุล" onChange={({ target: { value, name } }) => { handleOnChange(name, value) }} />
+                <InputStyle sizeinput={100} type="text" value={lastName} placeholder="นามสกุล" onChange={({ target: { value } }) => { setLastName(value) }} />
             </FormItemTextValidate>
 
             <FormItemTextValidate name="email" rules={[{ required: true, message: 'กรุณากรอกอีเมล' }]} >
-                <InputStyle sizeinput={100} type="email" value={userData.email} placeholder="อีเมล" onChange={({ target: { value, name } }) => { handleOnChange(name, value) }} />
+                <InputStyle sizeinput={100} type="email" value={email} placeholder="อีเมล" onChange={({ target: { value } }) => { setEmail(value) }} />
             </FormItemTextValidate>
 
             <FormItemTextValidate name="password" rules={[{ required: true, message: 'กรุณากรอกรหัสผ่าน' }]} >
-                <InputStyle sizeinput={100} type="password" value={userData.password} placeholder="รหัสผ่าน" onChange={({ target: { value, name } }) => { handleOnChange(name, value) }} />
+                <InputStyle sizeinput={100} type="password" value={password} placeholder="รหัสผ่าน" onChange={({ target: { value } }) => { setPassword(value) }} />
             </FormItemTextValidate>
 
             <FormItemTextValidate name="confirm"
@@ -92,7 +91,7 @@ export const RegisterForm = () => {
                     },
                     {
                         validator(_, value) {
-                            if (!value || userData.password === value) {
+                            if (!value || password === value) {
                                 return Promise.resolve();
                             }
                             return Promise.reject(new Error('รหัสผ่านที่กรอกไม่ตรงกัน โปรดลองอีกครั้ง'));
@@ -103,7 +102,7 @@ export const RegisterForm = () => {
             </FormItemTextValidate>
 
 
-            <ButtonStyle typebutton="Large" sizebutton={100} style={{ marginBottom: "15px" }} htmlType="submit" onClick={() => postUserData(userData.username, userData.firstName, userData.lastName, userData.email, userData.password)} >
+            <ButtonStyle typebutton="Large" sizebutton={100} style={{ marginBottom: "15px" }} htmlType="submit" onClick={API_register} >
                 สร้างบัญชี
             </ButtonStyle>
 
