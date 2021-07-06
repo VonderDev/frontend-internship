265f1a04d60b271b0dd60e089fed8b3ddf9cfb1e
Papichaya-Dev <lo0kpad@hotmail.com>
2021-07-06 16:58:53 +0700

    :tv:[created] multiple step form , Create post page 1 & 2 :tv:

--- src/components/pages/Board/shared/BoardCreate.styled.tsx
+++ src/components/pages/Board/shared/BoardCreate.styled.tsx
@@ -1,13 +1,24 @@
-import { Form, Input } from 'antd';
+import { Button, Form, Input } from 'antd';
 import styled from 'styled-components';
 
-export const InputCreateContent = styled(Input)`
+//----------- CREATE CONTENT PAGE 1 -----------//
+export const FormInputNameContent = styled(Input)`
     color: grey;
-    font-size: var(--font-18);
+    font-size: var(--font-16);
     border-radius: 15px;
     width: 80%;
-    height: 55px;
+    height: 45px;
     box-shadow: 0px 3px 6px #f1f1f1;
+`;
+
+export const FormInputContent = styled(Input)`
+    color: grey;
+    font-size: var(--font-16);
+    border-radius: 15px;
+    width: 80%;
+    height: 220px;
+    box-shadow: 0px 3px 6px #f1f1f1;
+    padding-bottom: 30%;
 `;
 
 export const BoardForm = styled(Form.Item)`
@@ -20,3 +31,48 @@ export const BoardForm = styled(Form.Item)`
 export const ContainerBoardCreate = styled.div`
     padding-left: 5%;
 `;
+
+export const ButtonGoNextCreateContent = styled.button`
+    border-radius: 12px;
+    font-weight: bolder;
+    color: white;
+    background-color: var(--Blue-400);
+    transform: translateY(70%);
+    margin-left: auto;
+    margin-right: auto;
+    width: 80%;
+    height: 7vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 16px;
+`;
+
+export const ButtonSummitPost = styled.button`
+    border-radius: 12px;
+    font-weight: bolder;
+    color: white;
+    background-color: var(--Green-400);
+    transform: translateY(160%);
+    margin-left: auto;
+    margin-right: auto;
+    width: 80%;
+    height: 7vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 16px;
+`;
+
+export const TextTopicContent = styled.div`
+    font-size: 16px;
+    color: var(--Gray-400);
+`;
+
+export const CountOfPage = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    transform: translateY(70%);
+`;
+//----------- CREATE CONTENT PAGE 2 -----------//
--- src/components/pages/Board/views/CreateContent/BoardCrateContent.tsx
+++ src/components/pages/Board/views/CreateContent/BoardCrateContent.tsx
@@ -1,11 +1,31 @@
-import { Col, Form, Row } from 'antd';
+import { Button, Form } from 'antd';
 import Container from 'components/Container/Container';
-import { InputCreateContent, BoardForm, ContainerBoardCreate } from '../../shared/BoardCreate.styled';
+import { BoardForm, ButtonGoNextCreateContent, ButtonSummitPost, ContainerBoardCreate, CountOfPage, FormInputContent, FormInputNameContent, TextTopicContent } from '../../shared/BoardCreate.styled';
 import { Upload } from 'antd';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import axios from 'axios';
+import { useHistory } from 'react-router-dom';
 
 function BoardCreateContent() {
+    const history = useHistory();
+    const [form, setForm] = useState({
+        email: '',
+        name: '',
+        password: '',
+    });
+    const [countPage, setCountPage] = useState(1);
+
+    const updateForm = (e: any) => {
+        setForm({
+            ...form,
+            [e.target.name]: e.target.value,
+        });
+    };
+
+    // useEffect(() => {
+    //     console.log(form);
+    //     console.log(form.name);
+    // }, [form]);
     //----------- CREATE FUNCTION UPLOAD IMAGE -----------//
     const [defaultFileList, setDefaultFileList] = useState([]);
 
@@ -13,10 +33,11 @@ function BoardCreateContent() {
         const { onSuccess, onError, file } = options;
         const formData = new FormData();
         const config = {
-            headers: { 'content-type': 'multipart/form-data' },
+            headers: { 'Content-Type': 'multipart/form-data' },
         };
         formData.append('image', file);
         try {
+            console.log('form data', file);
             const res = await axios.post('https://jsonplaceholder.typicode.com/posts', formData, config);
 
             onSuccess('Ok');
@@ -29,35 +50,63 @@ function BoardCreateContent() {
     };
 
     const handleOnChange = ({ file, fileList, event }: any) => {
-        console.log('[file, fileList, event]', fileList);
+        console.log('[FileList]:', fileList);
         //Using Hooks to update the state to the current filelist
         setDefaultFileList(fileList);
         //filelist - [{uid: "-1",url:'Some url to image'}]
     };
 
     return (
-        <Container header={{ title: 'สร้างกระทู้', right: 'menu', left: 'back' }}>
-            <ContainerBoardCreate>
-                <div>ชื่อกระทู้</div>
-                <Form initialValues={{ remember: true }} layout="horizontal">
-                    <BoardForm name="text" rules={[{ required: true, message: 'กรุณากรอกชื่อกระทู้ก่อนดำเนินการต่อ' }]}>
-                        <InputCreateContent type="text" placeholder="กรุณากรอกชื่อกระทู้" />
-                    </BoardForm>
-                </Form>
+        <>
+            <Container header={{ title: 'สร้างกระทู้', right: 'menu', left: 'back' }}>
+                {countPage === 1 ? (
+                    <>
+                        <ContainerBoardCreate>
+                            <TextTopicContent>ชื่อกระทู้</TextTopicContent>
+                            <Form initialValues={{ remember: true }}>
+                                <BoardForm name="text" rules={[{ required: true, message: 'กรุณากรอกชื่อกระทู้ก่อนดำเนินการต่อ' }]}>
+                                    <FormInputNameContent type="text" placeholder="กรุณากรอกชื่อกระทู้" />
+                                </BoardForm>
+                            </Form>
 
-                <div>รูปประกอบ (Optional)</div>
-                <Upload accept="image/*" customRequest={uploadImage} onChange={handleOnChange} listType="picture-card" defaultFileList={defaultFileList} className="image-upload-grid">
-                    {defaultFileList.length >= 1 ? null : <div>Upload image</div>}
-                </Upload>
-                {/* <Row>
-                    <Col span={24}>ชื่อกระทู้</Col>
-                </Row>
-                <Row>
-                    <Col span={24}>รูปประกอบ (Optional)</Col>
-                    <Row>sdsdsdd</Row>
-                </Row> */}
-            </ContainerBoardCreate>
-        </Container>
+                            <TextTopicContent>รูปประกอบ (Optional)</TextTopicContent>
+                            <Upload accept="image/*" customRequest={uploadImage} onChange={handleOnChange} listType="picture-card" defaultFileList={defaultFileList} className="image-upload-grid">
+                                {defaultFileList.length >= 2 ? null : <div>Upload image</div>}
+                            </Upload>
+                            <TextTopicContent>เนื้อหากระทู้</TextTopicContent>
+                            <Form initialValues={{ remember: true }}>
+                                <BoardForm name="text" rules={[{ required: true, message: 'กรุณากรอกเนื้อหากระทู้ก่อนดำเนินการต่อ' }]}>
+                                    <FormInputContent type="text" placeholder="กรุณากรอกเนื้อหาของกระทู้" />
+                                </BoardForm>
+                            </Form>
+                        </ContainerBoardCreate>
+                        <CountOfPage>{countPage} of 2</CountOfPage>
+                        <ButtonGoNextCreateContent type="submit" onClick={() => setCountPage(countPage + 1)} disabled={countPage > 1}>
+                            ดำเนินการต่อ
+                        </ButtonGoNextCreateContent>
+                    </>
+                ) : null}
+                {countPage === 2 ? (
+                    <>
+                        <ContainerBoardCreate>
+                            <TextTopicContent>ประเภทของกระทู้</TextTopicContent>
+                            <Button>บทความ</Button>
+                            <Button>คำถาม</Button>
+                            <TextTopicContent>แฮชเเท็กของกระทุ้</TextTopicContent>
+                            <Form initialValues={{ remember: true }}>
+                                <BoardForm name="text" rules={[{ required: true, message: 'กรุณากรอกชื่อกระทู้ก่อนดำเนินการต่อ' }]}>
+                                    <FormInputNameContent type="text" placeholder="กรุณาเลือกแฮชเเท็กของกระทู้" />
+                                </BoardForm>
+                            </Form>
+                        </ContainerBoardCreate>
+                        <ButtonSummitPost>สร้างกระทู้</ButtonSummitPost>
+                        <button className="btn btn-dark" type="submit" onClick={() => setCountPage(countPage - 1)} disabled={countPage < 2}>
+                            Back
+                        </button>
+                    </>
+                ) : null}
+            </Container>
+        </>
     );
 }
 
