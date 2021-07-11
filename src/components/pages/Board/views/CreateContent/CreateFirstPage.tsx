a39df78952d2ab8f820ba7e4f20bdd4a432a13d2
Papichaya-Dev <lo0kpad@hotmail.com>
2021-07-11 22:32:55 +0700

    :sparkles:[created] Board create content first&second page for child component :sparkles:

---
+++ src/components/pages/Board/views/CreateContent/CreateFirstPage.tsx
@@ -0,0 +1,62 @@
+import { FileImageTwoTone } from '@ant-design/icons';
+import { Form } from 'antd';
+import React from 'react';
+import {
+    ButtonGoNextCreateContent,
+    ContainerBoardCreate,
+    CountOfPageCreateContent,
+    CreateContentForm,
+    FormInputContent,
+    FormInputNameContent,
+    TextTopicContent,
+    UploadImage,
+} from '../../shared/BoardCreate.styled';
+
+interface CreateContentFirstPageProps {
+    updateContentData: (event: React.ChangeEvent<HTMLTextAreaElement> | React.ChangeEvent<HTMLInputElement>) => void;
+    uploadImage: (options: any) => void;
+    handleOnChangeFileImage: (info: any) => void;
+    defaultFileList: any;
+    countPage: number;
+    setCountPage: Function;
+}
+
+const CreateContentFirstPage: React.FC<CreateContentFirstPageProps> = ({ updateContentData, uploadImage, handleOnChangeFileImage, defaultFileList, countPage, setCountPage }) => {
+    return (
+        <>
+            <ContainerBoardCreate>
+                <TextTopicContent>ชื่อกระทู้</TextTopicContent>
+                <Form>
+                    <CreateContentForm name="nameContent" rules={[{ required: true, message: 'กรุณากรอกชื่อกระทู้ก่อนดำเนินการต่อ' }]}>
+                        <FormInputNameContent name="title" type="text" placeholder="กรุณากรอกชื่อกระทู้" onChange={updateContentData} />
+                    </CreateContentForm>
+                    <TextTopicContent>รูปประกอบ (Optional)</TextTopicContent>
+                    <UploadImage
+                        accept="image/*"
+                        customRequest={uploadImage}
+                        onChange={handleOnChangeFileImage}
+                        listType="picture-card"
+                        defaultFileList={defaultFileList}
+                        className="image-upload-grid"
+                    >
+                        {defaultFileList.length >= 2 ? null : (
+                            <div>
+                                <FileImageTwoTone style={{ fontSize: '35px' }} />
+                            </div>
+                        )}
+                    </UploadImage>
+                    <TextTopicContent>เนื้อหากระทู้</TextTopicContent>
+                    <CreateContentForm name="content" rules={[{ required: true, message: 'กรุณากรอกเนื้อหากระทู้ก่อนดำเนินการต่อ' }]}>
+                        <FormInputContent name="content_body" placeholder="กรุณากรอกเนื้อหาของกระทู้" onChange={updateContentData} />
+                    </CreateContentForm>
+                </Form>
+            </ContainerBoardCreate>
+            <CountOfPageCreateContent>{countPage} / 2</CountOfPageCreateContent>
+            <ButtonGoNextCreateContent htmlType="submit" onClick={() => setCountPage(countPage + 1)} disabled={countPage > 2}>
+                ดำเนินการต่อ
+            </ButtonGoNextCreateContent>
+        </>
+    );
+};
+
+export default CreateContentFirstPage;
