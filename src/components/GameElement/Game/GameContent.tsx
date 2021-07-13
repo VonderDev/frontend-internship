405aa175879073e77f44832e72b73b858a423794
Nichkamon Promputta <fainichkamon@gmail.com>
2021-07-13 16:57:45 +0700

    [Add] Asset Scene3 & function S3

--- src/components/GameElement/Game/GameContent.tsx
+++ src/components/GameElement/Game/GameContent.tsx
@@ -14,22 +14,28 @@ import treeb3 from '../Assets/Item/Tree/TreeB_3.png'
 import cloud1 from '../Assets/Item/Cloud/Cloud_1.png'
 import cloud2 from '../Assets/Item/Cloud/Cloud_2.png'
 import cloud3 from '../Assets/Item/Cloud/Cloud_3.png'
+import groundS3 from '../Assets/Background/bg_groundS3.png'
+import bgHouseS3 from '../Assets/Background/bg_houseS3.png'
+import door from '../Assets/Item/House/Door.png'
+import house from '../Assets/Item/House/house.png'
+import fence from '../Assets/Item/House/fenceHouse.png'
+import flower from '../Assets/Item/House/Flowers.png'
 
 const GameContent = (app: any,gameRef: any, updateRatioRef: any) => {
 
     const firstScene = new PIXI.Container();
     const secondScene = new PIXI.Container();
-    const doorSecene = new PIXI.Container();
-    const homeSecene = new PIXI.Container();
+    const doorScene = new PIXI.Container();
+    const homeScene = new PIXI.Container();
   
     secondScene.visible = false;
-    doorSecene.visible = false;
-    homeSecene.visible = false;
+    doorScene.visible = false;
+    homeScene.visible = false;
   
     app.stage.addChild(firstScene);
     app.stage.addChild(secondScene);
-    app.stage.addChild(doorSecene);
-
+    app.stage.addChild(homeScene);
+    app.stage.addChild(doorScene);
     //Preload asset
     window.onload = function(){
 
@@ -49,6 +55,12 @@ const GameContent = (app: any,gameRef: any, updateRatioRef: any) => {
             .add("cloud1", cloud1)
             .add("cloud2", cloud2)
             .add("cloud3", cloud3)
+            .add('groundS3',groundS3)
+            .add('bgHouseS3', bgHouseS3)
+            .add('door', door)
+            .add('house', house)
+            .add('fence', fence)
+            .add('flower', flower)
       loader.onProgress.add(showProgress);
       loader.onComplete.add(doneLoading);
       loader.onError.add(loadError);
@@ -72,11 +84,28 @@ const GameContent = (app: any,gameRef: any, updateRatioRef: any) => {
 
     }
 
+    const ticker = new PIXI.Ticker();
+    const cloud1Texture = PIXI.Texture.from(cloud1);
+    const Cloud1 = new PIXI.Sprite(cloud1Texture );
+    
+    const cloud2Texture = PIXI.Texture.from(cloud2);
+    const Cloud2 = new PIXI.Sprite(cloud2Texture );
+    
+    function animate() {
+      console.log('Width',firstScene.width)
+      if(Cloud1.x + Cloud1.width < firstScene.width){
+        Cloud1.x += 0.1 *2
+      }
+      if(Cloud2.x + Cloud2.width < firstScene.width){
+        Cloud2.x -= 0.1 * 3
+      }
+    }
+
     function onClick1() {
       console.log("chang to Scene2")
       firstScene.visible = false;
-      doorSecene.visible = false;
-      homeSecene.visible = false;
+      doorScene.visible = false;
+      homeScene.visible = false;
       secondScene.visible = true;
       secondScene.interactive = true;
       secondScene.buttonMode = true;
@@ -85,6 +114,28 @@ const GameContent = (app: any,gameRef: any, updateRatioRef: any) => {
     }
     function onClick2() {
       console.log("chang to Scene3")
+      firstScene.visible = false;
+      doorScene.visible = false;
+      homeScene.visible = true;
+      secondScene.visible = false;
+      homeScene.interactive = true;
+      homeScene.buttonMode = true;
+      homeScene.on('pointerdown', onClick3);
+      gameScene3();
+    }
+    function onClick3() {
+      console.log("chang to doorScene")
+      firstScene.visible = false;
+      doorScene.visible = true;
+      homeScene.visible = false;
+      secondScene.visible = false;
+      doorScene.interactive = true;
+      doorScene.buttonMode = true;
+      doorScene.on('pointerdown', onClick4);
+      doorScene3();
+    }
+    function onClick4() {
+      console.log("chang to inHome")
     }
 
     function gameScene1() {
@@ -106,19 +157,28 @@ const GameContent = (app: any,gameRef: any, updateRatioRef: any) => {
       bgTree.anchor.set(0,-1);
       firstScene.addChild(bgTree)
 
-      const cloud1Texture = PIXI.Texture.from(cloud1);
-      const Cloud1 = new PIXI.Sprite(cloud1Texture );
       Cloud1.scale.set(0.5);
-      Cloud1.position.set(250, 100);
+      Cloud1.position.set(10, 20);
       firstScene.addChild(Cloud1)
 
-      const cloud2Texture = PIXI.Texture.from(cloud2);
-      const Cloud2 = new PIXI.Sprite(cloud2Texture );
       Cloud2.scale.set(0.5);
-      Cloud2.position.set(250, 100);
+      Cloud2.position.set(500, 150);
       firstScene.addChild(Cloud2)
 
       //Item 
+
+      const treeb2Texture = PIXI.Texture.from(treeb2);
+      const Treeb2 = new PIXI.Sprite(treeb2Texture);
+      Treeb2.scale.set(0.3);
+      Treeb2.position.set(150,230);
+      firstScene.addChild(Treeb2)
+
+      const treeb1Texture = PIXI.Texture.from(treeb1);
+      const Treeb1 = new PIXI.Sprite(treeb1Texture);
+      Treeb1.scale.set(0.5);
+      Treeb1.position.set(-20,300);
+      firstScene.addChild(Treeb1)
+
       const treef1Texture = PIXI.Texture.from(treef1);
       const Treef1 = new PIXI.Sprite(treef1Texture);
       Treef1.scale.set(0.8);
@@ -131,7 +191,6 @@ const GameContent = (app: any,gameRef: any, updateRatioRef: any) => {
       Treef2.position.set(850,0);
       Treef2.scale.x = -1
       firstScene.addChild(Treef2)
-
 
       //left
       const bush2Texture = PIXI.Texture.from(bush2);
@@ -163,7 +222,15 @@ const GameContent = (app: any,gameRef: any, updateRatioRef: any) => {
       Bush4.scale.set(0.4);
       Bush4.position.set(450,620);
       firstScene.addChild(Bush4)
+
+      ticker.add(animate);
+      ticker.start();
+
+      setTimeout(()=>{
+        ticker.stop();
+        }, 30000)
     }
+
 
     function gameScene2 () {
       //Background 
@@ -184,31 +251,105 @@ const GameContent = (app: any,gameRef: any, updateRatioRef: any) => {
       bgTree.anchor.set(0,-1);
       secondScene.addChild(bgTree)
 
+      Cloud1.scale.set(0.5);
+      Cloud1.position.set(10, 20);
+      secondScene.addChild(Cloud1)
+
+      Cloud2.scale.set(0.5);
+      Cloud2.position.set(500, 150);
+      secondScene.addChild(Cloud2)
       //Item 
+      const treeb1Texture = PIXI.Texture.from(treeb1);
+      const Treeb1 = new PIXI.Sprite(treeb1Texture);
+      Treeb1.scale.set(0.4)
+      Treeb1.position.set(250,400);
+      secondScene.addChild(Treeb1)
+
+      const treeb3Texture = PIXI.Texture.from(treeb3);
+      const Treeb3 = new PIXI.Sprite(treeb3Texture);
+      Treeb3.scale.set(0.3)
+      Treeb3.position.set(160,250);
+      secondScene.addChild(Treeb3)
+
+      const treeb2Texture = PIXI.Texture.from(treeb2);
+      const Treeb2 = new PIXI.Sprite(treeb2Texture);
+      Treeb2.scale.set(0.4)
+      Treeb2.position.set(0,300);
+      secondScene.addChild(Treeb2)
+
       const treef1Texture = PIXI.Texture.from(treef1);
       const Treef1 = new PIXI.Sprite(treef1Texture);
-      Treef1.scale.set(0.8);
-      Treef1.position.set(-300,-100);
+      Treef1.scale.set(0.7)
+      Treef1.position.set(300,80);
       secondScene.addChild(Treef1)
+
+      const bush3Texture = PIXI.Texture.from(bush3);
+      const Bush3 = new PIXI.Sprite(bush3Texture);
+      Bush3.scale.set(0.3);
+      Bush3.position.set(-80,600);
+      secondScene.addChild(Bush3)
 
       const treef2Texture = PIXI.Texture.from(treef2);
       const Treef2 = new PIXI.Sprite(treef2Texture);
-      Treef2.scale.set(0.8)
-      Treef2.position.set(700,-200);
-      Treef2.scale.x = -1
+      Treef2.scale.set(0.9);
+      Treef2.position.set(-250,-100);
       secondScene.addChild(Treef2)
+
+      const bush4Texture = PIXI.Texture.from(bush4);
+      const Bush4 = new PIXI.Sprite(bush4Texture);
+      Bush4.scale.set(0.3);
+      Bush4.position.set(-5,700);
+      secondScene.addChild(Bush4)
 
       const bush2Texture = PIXI.Texture.from(bush2);
       const Bush2 = new PIXI.Sprite(bush2Texture);
-      Bush2.scale.set(0.4);
-      Bush2.position.set(-50,500);
+      Bush2.scale.set(0.5);
+      Bush2.position.set(350,550);
       secondScene.addChild(Bush2)
 
       const bush1Texture = PIXI.Texture.from(bush1);
       const Bush1 = new PIXI.Sprite(bush1Texture);
-      Bush1.scale.set(0.7);
-      Bush1.position.set(-100,600);
+      Bush1.scale.set(0.6);
+      Bush1.position.set(450,600);
       secondScene.addChild(Bush1)
+    }
+
+    function gameScene3 () {
+      //Background 
+      const skyTexture = PIXI.Texture.from(sky);
+      const bgSky = new PIXI.Sprite(skyTexture);
+      bgSky.scale.set(0.7);
+      bgSky.anchor.set(0,0);
+      homeScene.addChild(bgSky)
+
+      const texture = PIXI.Texture.from(groundS3);
+      const dirtS3 = new PIXI.Sprite(texture);
+      dirtS3.scale.set(0.7);
+      dirtS3.anchor.set(0,-0.5);
+      homeScene.addChild(dirtS3)
+
+      const houseTexture = PIXI.Texture.from(house);
+      const houseS3 = new PIXI.Sprite(houseTexture);
+      houseS3.scale.set(0.5);
+      houseS3.position.set(100,200);
+      homeScene.addChild(houseS3)
+
+      const treef2Texture = PIXI.Texture.from(treef2);
+      const Treef2 = new PIXI.Sprite(treef2Texture);
+      Treef2.scale.set(0.7)
+      Treef2.position.set(400,0);
+      Treef2.scale.x = -1
+      firstScene.addChild(Treef2)
+
+    }
+    function doorScene3 () {
+            //Background 
+            const bghouseTexture = PIXI.Texture.from(bgHouseS3);
+            const bghouse = new PIXI.Sprite(bghouseTexture);
+            bghouse.scale.set(0.7);
+            bghouse.anchor.set(0,0);
+            doorScene.addChild(bghouse)
+    
     }
 
     // const onMainResize = (width:number , height:number) => {
--- src/components/GameElement/Game/testpixi.tsx
+++ src/components/GameElement/Game/testpixi.tsx
@@ -1,4 +1,5 @@
 import Container from "components/Container/Container";
+import { Box } from "shared/style/theme/component";
 import { AppProvider } from "../PixiStore/AppContext";
 import PixiApp from "../PixiStore/PixiApp";
 import { PixiProvider } from "../PixiStore/PixiContext";
@@ -10,9 +11,18 @@ const TestPixi = () => {
             <AppProvider>
             <PixiProvider>
                 <PixiApp content={GameContent}/>
-                <div style={{width:'200px' , height: '200px' ,backgroundColor: 'wheat' ,position: 'absolute', top: '80px', display: 'inline-block',    opacity: 0.5}}>
+                <Box justify="center" align="center" direction="row">
+                <div style={{width:'200px' 
+                , height: '200px'
+                ,backgroundColor: 'wheat'
+                ,position: 'absolute'
+                , top: '80px'
+                , display: 'inline-block'
+                , opacity: 0.5}}>
+
                     <h1>This is React Content</h1>
                 </div>
+                </Box>
             </PixiProvider>
             </AppProvider>
         </Container>
