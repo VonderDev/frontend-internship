6abd7671226a7515870815fd4a09a2b86b6a6a6e
Nichkamon Promputta <fainichkamon@gmail.com>
2021-07-23 06:35:23 +0700

    [clean code]

--- src/components/pages/Test/views/TestStartPage/ButtonLoadingStart.tsx
+++ src/components/pages/Test/views/TestStartPage/ButtonLoadingStart.tsx
@@ -1,12 +1,7 @@
 import { Spin } from 'antd';
-import { useState ,useEffect } from 'react';
+import { useState } from 'react';
 import { ButtonLoading, ContainerButtonLoading, IsLoadingSpinner } from '../../shared/styles/Test/TestPage.styled';
 import { useHistory } from 'react-router-dom';
-import { ApiGetTestData } from '../../apis/test.api';
-import { IQuestion } from '../../shared/interface/Test.interfaces';
-import { useAuthContext } from 'components/AuthContext/AuthContext';
-import { cleanup } from '@testing-library/react';
-import { ApiPostTestResult } from '../../apis/test.api';
 import { ApiPostCreateGuestToken } from '../../apis/GuestTest.api';
 
 function ButtonLoadingStart() {
@@ -26,29 +21,6 @@ function ButtonLoadingStart() {
             history.push('/testquestion');
         }, 1500);
     };
-
-    //------------------- GET USERNAME FOR CHECK LIKE -------------------//
-    const { getUser } = useAuthContext();
-    const [userId, setUserId] = useState('');
-
-    const getUserId = async () => {
-        const token = localStorage.getItem('token');
-        const response = await getUser();
-        if (token) {
-            if (response) {
-                setUserId(response._id);
-                console.log(userId);
-            } else {
-                console.log('error');
-            }
-        } else {
-            console.log('No token');
-        }
-    };
-
-    function postGenTokenForGuest() {
-        ApiPostTestResult;
-    }
 
     return (
         <ContainerButtonLoading>
